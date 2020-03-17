609596a20d75b6d9385ab64b084300f634b06324
Raul Garcia <punkvomit21@gmail.com>
2020-03-16 23:56:35 -0400

    add generate park results, fix displayparkresults

--- index.js
+++ index.js
@@ -1,57 +1,49 @@
 
 /* global $ */
-// 'https://developer.nps.gov/api/v1/parks?parkCode=acad'
 
 
 const apiKey = 'fAvwKXa8AAXHrlhhywnH2TgzzE4pSjTjhpZ1QPwc';
-//const searchUrl = 'https://developer.nps.gov/api/v1/alerts';
-const searchUrl = 'api.nps.gov/api/v1/parks?stateCode=,fl,&limit=10';
-// const stateSearchUrl = 'api.nps.gov/api/v1/parks?stateCode=,fl&limit=10';
+const searchUrl = 'https://developer.nps.gov/api/v1/parks';
 
-// function formatQueryParams(params) {
-//   const queryItems = Object.keys(params)
-//     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
-//   return queryItems.join('&');
-// }
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+  return queryItems.join('&');
+}
 
 function displayParkResults(responseJson) {
-  (responseJson);
-  $('#js-results-list').empty();
-  for (let i = 0; i < responseJson.items.length; i++) {
-    $('#js-results-list').append(
-      `<li><h3>${responseJson.items[i].snippet.title}</h3>
-      <p>${responseJson.items[i].snippet.description}</p>
-      <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
-      </li>`
-    );
-  }
+
+  $('#js-results-list').html(generateParkResults(responseJson.data));
   $('#js-results').removeClass('hidden');
 }
 
 
+function generateParkResults(data) {
+  let html = '';
+  data.forEach(item => {
+    html += `
+    <li><h3>${item.fullName}</h3>
+      <p>${item.description}</p>
+      </li>
+    `;
+  });
+  return html;
+}
 
 
-// function getNatlParks(stateCode, maxResults = 10) {
-//   const params = {
-//     key: apiKey,
-//     q: stateCode,
-//     maxResults,
-//     type: 'string'
-//   };
 
-// const queryString = formatQueryParams(params);
-// const url = searchUrl + '?' + queryString;
-function getNatlParks() {
-  fetch(searchUrl, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': apiKey
-    },
-    body: JSON.stringify({
+function getNatlParks(stateCode, limit = 10) {
+  const params = {
+    'api_key': apiKey,
+    stateCode,
+    limit,
+  };
 
-    })
-  })
+  const queryString = formatQueryParams(params);
+  const url = searchUrl + '?' + queryString;
+
+
+  fetch(url)
     .then(response => {
       if (response.ok) {
         return response.json();
